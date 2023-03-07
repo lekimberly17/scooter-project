0a65f8b28946ce67a21db6e23e8d9496c7c30e6a
corumgold <goldcorum@gmail.com>
2023-03-07 12:35:28 -0500

    Code review

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -1,5 +1,5 @@
-const User = require('./User')
-const Scooter = require('./Scooter')
+const User = require("./User");
+const Scooter = require("./Scooter");
 
 class ScooterApp {
   // ScooterApp code here
@@ -7,112 +7,111 @@ class ScooterApp {
     this.stations = {
       "Station A": [],
       "Station B": [],
-      "Station C": []
-    }
-    this.registeredUsers = {}
+      "Station C": [],
+    };
+    this.registeredUsers = {};
   }
 
   // registerUser(username, password, age)
   registerUser(username, password, age) {
+    //I like the use of separate if statements here, some would argue that separate if statements are better than if/else because they are easier to read.
     if (this.registeredUsers[username]) {
-      throw new Error("Already registered")
+      throw new Error("Already registered");
     }
     if (age < 18) {
-      throw new Error("Too young to register")
+      throw new Error("Too young to register");
     }
-    const user = new User(username, password, age)
-    this.registeredUsers[username] = user
-    console.log(`User ${username} has been registered`)
-    return user
+    const user = new User(username, password, age);
+    //It looks like you are using the user's username as the key, this works as long as usernames are required to always be unique, otherwise it may make more sense to use the userId as the key
+    this.registeredUsers[username] = user;
+    console.log(`User ${username} has been registered`);
+    return user;
   }
 
   // loginUser(username, password)
   loginUser(username, password) {
-    const user = this.registeredUsers[username]
+    const user = this.registeredUsers[username];
     if (user) {
-      user.login(password)
-      console.log(`User ${username} has been logged in`)
+      user.login(password);
+      console.log(`User ${username} has been logged in`);
     } else {
-      throw new Error("Username or password is incorrect")
+      throw new Error("Username or password is incorrect");
     }
   }
 
   // logoutUser(username)
   logoutUser(username) {
-    const user = this.registeredUsers[username]
+    const user = this.registeredUsers[username];
     if (user) {
-      user.logout()
-      console.log(`User ${username} has been logged out`)
+      user.logout();
+      console.log(`User ${username} has been logged out`);
     } else {
-      throw new Error("No such user is logged in")
+      throw new Error("No such user is logged in");
     }
   }
 
   // createScooter(station)
   createScooter(station) {
     if (!this.stations[station]) {
-      throw new Error("No such station")
+      throw new Error("No such station");
     }
-    const scooter = new Scooter(station)
-    this.stations[station].push(scooter)
-    console.log(`Created new scooter: ${scooter.serial}`)
-    return scooter
+    const scooter = new Scooter(station);
+    this.stations[station].push(scooter);
+    console.log(`Created new scooter: ${scooter.serial}`);
+    return scooter;
   }
 
   // dockScooter(scooter, station)
   dockScooter(scooter, stationName) {
     if (!this.stations[stationName]) {
-      throw new Error("No such station")
+      throw new Error("No such station");
     }
-  
+
     if (scooter.station === stationName) {
-      throw new Error("Scooter already at station")
+      throw new Error("Scooter already at station");
     }
-  
-    const index = this.stations[scooter.station].indexOf(scooter)
+
+    const index = this.stations[scooter.station].indexOf(scooter);
     if (index !== -1) {
-      this.stations[scooter.station].splice(index, 1)
+      this.stations[scooter.station].splice(index, 1);
     }
-  
-    this.stations[stationName].push(scooter)
-    scooter.station = stationName
-    
+
+    this.stations[stationName].push(scooter);
+    scooter.station = stationName;
+
     console.log(`Scooter ${scooter.serial} is docked at ${stationName}`);
   }
-  
-  
+
   // rentScooter(scooter, user)
   rentScooter(scooter, user) {
     if (scooter.user) {
-      throw new Error("Scooter already rented")
+      throw new Error("Scooter already rented");
     }
-    const stationKeys = Object.keys(this.stations)
+    const stationKeys = Object.keys(this.stations);
     for (const stationKey of stationKeys) {
-      const station = this.stations[stationKey]
-      const index = station.indexOf(scooter)
+      const station = this.stations[stationKey];
+      const index = station.indexOf(scooter);
       if (index >= 0) {
-        station.splice(index, 1)
-        break
+        station.splice(index, 1);
+        break;
       }
     }
-    scooter.rent(user)
-    console.log(`Scooter ${scooter.serial} is rented`)
+    scooter.rent(user);
+    console.log(`Scooter ${scooter.serial} is rented`);
   }
 
   // print()
   print() {
-    console.log("Registered users:")
+    console.log("Registered users:");
     for (const username in this.registeredUsers) {
-      console.log(`- ${username}`)
+      console.log(`- ${username}`);
     }
-    console.log("Stations:")
+    console.log("Stations:");
     for (const stationName in this.stations) {
-      const scooters = this.stations[stationName]
-      console.log(`- ${stationName}: ${scooters.length} scooter(s)`)
+      const scooters = this.stations[stationName];
+      console.log(`- ${stationName}: ${scooters.length} scooter(s)`);
     }
   }
-
 }
 
-
-module.exports = ScooterApp
+module.exports = ScooterApp;
